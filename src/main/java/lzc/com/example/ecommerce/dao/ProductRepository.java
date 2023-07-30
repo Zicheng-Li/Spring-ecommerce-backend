ff1f0ea273a0507fd673ecc06b28935814a36fc1
ZL&EL <lzc123456ab@gmail.com>
2023-07-29 20:46:08 -0400

    1

--- src/main/java/lzc/com/example/ecommerce/dao/ProductRepository.java
+++ src/main/java/lzc/com/example/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package lzc.com.example.ecommerce.dao;
 
 import lzc.com.example.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
