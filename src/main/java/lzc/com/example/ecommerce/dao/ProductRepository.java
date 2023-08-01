305876c76aa4592ea4ea070d75f395772b7bdcec
ZL&EL <lzc123456ab@gmail.com>
2023-07-31 22:02:48 -0400

    1

--- src/main/java/lzc/com/example/ecommerce/dao/ProductRepository.java
+++ src/main/java/lzc/com/example/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,16 @@
 package lzc.com.example.ecommerce.dao;
 
 import lzc.com.example.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // spring data jpa will automatically implement this method, it is the query method, start by findBy, quire by the field name
+    // spring will expose the endpoint to the frontend, and the frontend call findByCategoryId
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
