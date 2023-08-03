410a2f69c9c255e3978107f77360eb086c2b7fcd
ZL&EL <lzc123456ab@gmail.com>
2023-08-02 22:51:29 -0400

    1

--- src/main/java/lzc/com/example/ecommerce/dao/ProductRepository.java
+++ src/main/java/lzc/com/example/ecommerce/dao/ProductRepository.java
@@ -13,4 +13,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // spring data jpa will automatically implement this method, it is the query method, start by findBy, quire by the field name
     // spring will expose the endpoint to the frontend, and the frontend call findByCategoryId
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
