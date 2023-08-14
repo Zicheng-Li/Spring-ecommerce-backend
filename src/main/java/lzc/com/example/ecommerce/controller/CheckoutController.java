e4e57e025a0cce52b5172148b6e7e2d512bfea5d
ZL&EL <lzc123456ab@gmail.com>
2023-08-14 14:48:36 -0400

    1

---
+++ src/main/java/lzc/com/example/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,24 @@
+package lzc.com.example.ecommerce.controller;
+
+import lzc.com.example.ecommerce.dto.Purchase;
+import lzc.com.example.ecommerce.dto.PurchaseResponse;
+import lzc.com.example.ecommerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+    private CheckoutService service;
+
+    @Autowired
+    public CheckoutController(CheckoutService service) {
+        this.service = service;
+    }
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+      PurchaseResponse response = service.placeOrder(purchase);
+      return response;
+    }
+}
