6bb169063e04ad4fa4b271f45d5fc025c6c36634
ZL&EL <lzc123456ab@gmail.com>
2023-08-21 19:49:22 -0400

    1

--- src/main/java/lzc/com/example/ecommerce/controller/CheckoutController.java
+++ src/main/java/lzc/com/example/ecommerce/controller/CheckoutController.java
@@ -1,10 +1,18 @@
 package lzc.com.example.ecommerce.controller;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import lzc.com.example.ecommerce.dto.PaymentInfo;
 import lzc.com.example.ecommerce.dto.Purchase;
 import lzc.com.example.ecommerce.dto.PurchaseResponse;
 import lzc.com.example.ecommerce.service.CheckoutService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -20,5 +28,13 @@ public class CheckoutController {
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
       PurchaseResponse response = service.placeOrder(purchase);
       return response;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo info) throws StripeException {
+        PaymentIntent intent = service.createPaymentIntent(info);
+
+        String paymentStr= intent.toJson();
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
